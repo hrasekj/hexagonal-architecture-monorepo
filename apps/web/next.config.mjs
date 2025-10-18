6b44bc3f7bcc3dcd91fa631b6963dc1c6baa8eeb
Jakub Hrášek <hrasekj@gmail.com>
2025-10-18 12:45:26 +0200

    chore: export nextjs as standalone app

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@web/ui"],
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
