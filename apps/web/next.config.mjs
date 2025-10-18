92326c665e7523e54f8981baafb36eb9134f46b4
Jakub Hrášek <hrasekj@gmail.com>
2025-10-18 13:26:10 +0200

    chore(aws): make nextjs build ssr

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@web/ui"],
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // trailingSlash: true,
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
