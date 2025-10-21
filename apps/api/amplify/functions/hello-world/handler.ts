52a20405699b4c154942f152c196c9e16011a888
Jakub Hrášek <hrasekj@gmail.com>
2025-10-21 22:27:02 +0200

    fix: deploy issues

--- apps/api/amplify/functions/hello-world/handler.ts
+++ apps/api/amplify/functions/hello-world/handler.ts
@@ -9,6 +9,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     headers: {
       'Access-Control-Allow-Origin': '*', // Restrict this to domains you trust
       'Access-Control-Allow-Headers': '*', // Specify only the headers you need to allow
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ currentDate: new Date().toISOString(), content: 'Hello, World!' }),
   }
